9bb4cac1fb6598a21ec1ae0c3f81326a62bd879c
Tyler Twining <twijn@twijn.net>
2022-12-27 20:45:23 -0600

    Implement slash commands and interaction listener

---
+++ discord/index.js
@@ -0,0 +1,75 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const { Client, Collection, Events, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const config = require('../config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// create collections
+client.commands = new Collection();
+client.interactionListeners = new Collection();
+
+// populate collections
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+const interactionsPath = path.join(__dirname, 'interactionListeners');
+const interactionFiles = fs.readdirSync(interactionsPath).filter(file => file.endsWith('.js'));
+
+for (const file of interactionFiles) {
+	const filePath = path.join(interactionsPath, file);
+	const listener = require(filePath);
+	// Set a new item in the Collection with the key as the listener name and the value as the exported module
+	if ('name' in listener && 'verify' in listener && 'execute' in listener) {
+		client.interactionListeners.set(listener.name, listener);
+	} else {
+		console.log(`[WARNING] The interaction listener at ${filePath} is missing a required "name", "verify", or "execute" property.`);
+	}
+}
+
+// ready message
+client.once(Events.ClientReady, c => {
+	console.log(`Discord ready! Logged in as ${c.user.tag}`);
+});
+
+// interaction listener
+client.on(Events.InteractionCreate, interaction => {
+	interaction.success = message => {
+		interaction.reply({
+			embeds: [new EmbedBuilder().setTitle("Successful!").setColor(0x2dce3d).setDescription(message)],
+			ephemeral: true,
+		});
+	}
+	interaction.error = message => {
+		interaction.reply({
+			embeds: [new EmbedBuilder().setTitle("An error occurred!").setColor(0xd63939).setDescription(message)],
+			ephemeral: true,
+		});
+	}
+
+    client.interactionListeners.forEach(listener => {
+		try {
+			if (listener.verify(interaction)) listener.execute(interaction);
+		} catch (err) {
+			console.error(err);
+		}
+	});
+});
+
+global.discord = client;
+
+client.login(config.discord.token);
+
+require("./postSlashCommands");
---
+++ discord/interactionListeners/commandListener.js
@@ -0,0 +1,21 @@
+const listener = {
+    name: "commandListener",
+    verify: interaction => interaction.isChatInputCommand(),
+    execute: async interaction => {
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+}
+
+module.exports = listener;
