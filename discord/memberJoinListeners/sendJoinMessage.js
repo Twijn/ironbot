ff383b2325a7cc7f647476bb5efb5bbc5ca657a6
Tyler Twining <twijn@twijn.net>
2023-01-20 11:25:01 -0500

    Join message in welcome chat

---
+++ discord/memberJoinListeners/sendJoinMessage.js
@@ -0,0 +1,28 @@
+const { GuildMember, EmbedBuilder } = require("discord.js");
+const config = require("../../config.json");
+
+const listener = {
+    name: "sendJoinDM",
+    /**
+     * Executor for this listener
+     * @param {GuildMember} member 
+     */
+    execute: async member => {
+        const embed = new EmbedBuilder()
+            .setTitle(config.join.title.replace("{{username}}", member.displayName))
+            .setDescription(
+                config.join.messages[Math.floor(Math.random()*config.join.messages.length)]
+                    .replace("{{username}}", member.displayName))
+            .setAuthor({
+                name: member.displayName,
+                iconURL: member.displayAvatarURL(),
+            })
+            .setThumbnail(member.guild.iconURL({size: 128}))
+            .setColor(0xf28227);
+        
+        member.guild.channels.cache.get(config.join.channel)
+            .send({embeds: [embed]});
+    }
+}
+
+module.exports = listener;
