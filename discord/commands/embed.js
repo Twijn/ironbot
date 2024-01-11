0cfd0f7c64af6954909639bbcd3c4c72cbbfd8d9
Tyler Twining <twijn@twijn.net>
2024-01-11 16:18:40 -0500

    Remove unused commands

--- discord/commands/embed.js
+++ discord/commands/embed.js
@@ -9,11 +9,6 @@ const command = {
                 .setName("roles")
                 .setDescription("Designed embed for role selection")
         )
-        .addSubcommand(
-            new SlashCommandSubcommandBuilder()
-                .setName("zomboid")
-                .setDescription("Sends Project Zomboid server details")
-        )
         .setDMPermission(false),
     /**
      * Executor for this chat command
@@ -72,40 +67,6 @@ const command = {
                         .catch(console.error);
                 })
                 .catch(console.error);
-        } else if (subcommand === "zomboid") {
-            const embed = new EmbedBuilder()
-                .setTitle("Project Zomboid Server Details")
-                .setDescription("**Project Zomboid Community Server**\nAccess to the `Project Zomboid` server can be requested using `/zomboid request`, with the `username` field being your requested PZ username.")
-                .setColor(0xcf7b2d)
-                .setFields([
-                    {
-                        name: "Server IP",
-                        value: codeBlock("zomboid.autumnsdawn.net"),
-                        inline: true,
-                    },
-                    {
-                        name: "Port",
-                        value: codeBlock("16261"),
-                        inline: true,
-                    },
-                    {
-                        name: "Server Password",
-                        value: codeBlock("R3C9q9FY"),
-                        inline: true,
-                    },
-                    {
-                        name: "User Credentials",
-                        value: "User credentials must be requested by a moderator.\nPlease do so using the following command:" + codeBlock("/zomboid request") + "Note: Commands are not allowed in this channel. Please run the command [here](https://discord.com/channels/859283517451010088/1083050189822107649).",
-                        inline: false,
-                    },
-                ]);
-
-                interaction.channel.send({embeds: [embed]})
-                    .then(message => {
-                        interaction.success("Sent the message!")
-                            .catch(console.error);
-                    })
-                    .catch(console.error);
         } else {
             interaction.error("Unknown embed!")
                 .catch(console.error);
