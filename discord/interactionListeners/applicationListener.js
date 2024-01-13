98061d57c822797debb07ea71e74a6b2f9b26b2e
Tyler Twining <twijn@twijn.net>
2024-01-13 13:52:39 -0500

    Additional editing options on Discord

--- discord/interactionListeners/applicationListener.js
+++ discord/interactionListeners/applicationListener.js
@@ -1,4 +1,4 @@
-const { ButtonInteraction } = require("discord.js");
+const { ButtonInteraction, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require("discord.js");
 
 const utils = require("../../utils/");
 
@@ -39,7 +39,49 @@ const listener = {
                 interaction.error(String(err));
             })
         } else if (type === "denyapp") {
+            const modal = new ModalBuilder()
+                .setCustomId(`deny-${application._id}`)
+                .setTitle("Set Deny Reason(s)")
+                .addComponents(
+                    new ActionRowBuilder()
+                        .addComponents(
+                            new TextInputBuilder()
+                                .setCustomId("user")
+                                .setLabel("User Reason")
+                                .setPlaceholder("This reason is viewable by the user.")
+                                .setRequired(true)
+                                .setMinLength(3)
+                                .setMaxLength(100)
+                                .setStyle(TextInputStyle.Short)
+                        ),
+                    new ActionRowBuilder()
+                        .addComponents(
+                            new TextInputBuilder()
+                                .setCustomId("operator")
+                                .setLabel("Operator Reason")
+                                .setPlaceholder("Viewable by operators. Defaults to the user reason")
+                                .setRequired(false)
+                                .setMinLength(3)
+                                .setMaxLength(100)
+                                .setStyle(TextInputStyle.Short)
+                        )
+                );
 
+            interaction.showModal(modal);
+            interaction.awaitModalSubmit({
+                time: 15 * 60 * 1000,
+                filter: interaction => interaction.customId === `deny-${application._id}`,
+            }).then(modalInteraction => {
+                const userReason = modalInteraction.fields.getTextInputValue("user");
+                const operatorReason = modalInteraction.fields.getTextInputValue("operator");
+
+                application.deny(userReason, operatorReason).then(() => {
+                    modalInteraction.reply({content: "Successfully denied application!", ephemeral: true});
+                }, err => {
+                    console.error(err);
+                    modalInteraction.reply({content: String(err), ephemeral: true});
+                });
+            }).catch(console.error);
         } else {
             interaction.error(`Unknown action type \`${type}\``);
         }
