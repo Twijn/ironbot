09c5019c77460d987beee60ecd86bd7d743731ec
Tyler Twining <twijn@twijn.net>
2024-11-04 11:25:43 -0500

    Update discord & twitch cache time frames

--- utils/cache/CacheManager.js
+++ utils/cache/CacheManager.js
@@ -6,9 +6,9 @@ const ONE_HOUR = 60 * 60 * 1000;
 
 class CacheManager {
 
-    discord = new Cache(ONE_HOUR);
+    discord = new Cache(8 * ONE_HOUR);
 
-    twitch = new Cache(ONE_HOUR);
+    twitch = new Cache(8 * ONE_HOUR);
 
     session = new Cache(8 * ONE_HOUR);
 
