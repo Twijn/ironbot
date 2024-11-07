0fe7d43de3e25be0428018eb915ab3db680d3d90
Tyler Twining <twijn@twijn.net>
2024-11-06 23:11:20 -0500

    Replace discord user for Members page with profiles

--- web/controllers/members.js
+++ web/controllers/members.js
@@ -4,8 +4,22 @@ const router = express.Router();
 const utils = require("../../utils");
 
 router.get("/", async (req, res) => {
+    const finalMembers = [];
+    const membersByRole = await utils.MemberManager.getMembersByRole(true);
+
+    for (let i = 0; i < membersByRole.length; i++) {
+        let members = [];
+        for (let x = 0; x < membersByRole[i].members.length; x++) {
+            members.push(await membersByRole[i].members[x].identity.getProfile());
+        }
+        finalMembers.push({
+            role: membersByRole[i].role,
+            members,
+        });
+    }
+
     res.render("pages/members", {
-        members: await utils.MemberManager.getMembersByRole(true),
+        members: finalMembers,
         twitchUsers: req.twitchUsers,
         discordUsers: req.discordUsers,
         identity: req?.session?.identity,
